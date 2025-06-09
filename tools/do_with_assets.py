0d6682e33956cea48dc9fde835d0dd02ca165474
KyleDeng <dlkxiaok@163.com>
2025-06-09 18:48:20 +0800

    fix: assets build

--- tools/do_with_assets.py
+++ tools/do_with_assets.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import json
 import shutil
 
 from tools.util import (
@@ -11,35 +12,9 @@ from tools.util import (
 
 
 def clean(build_root, target):
-    # rm elf and bin
-    cp0_build_root = os.path.join(build_root, target)
-    cp0_app_elf = os.path.join(cp0_build_root, "app.elf")
-    cp0_app_bin = os.path.join(cp0_build_root, "app.bin")
-    cp0_elf_c_o = os.path.join(cp0_build_root, "project_elf_src.c.obj")
-    cp1_app1_bin = os.path.join(cp0_build_root, "app1.bin")
-    app_all_bin = os.path.join(cp0_build_root, "app-all.bin")
-
-    cp1_build_root = os.path.join(build_root, f"{target}_cp1")
-    cp1_app_elf = os.path.join(cp1_build_root, "app.elf")
-    cp1_app_bin = os.path.join(cp1_build_root, "app.bin")
-    cp1_elf_c_o = os.path.join(cp1_build_root, "project_elf_src.c.obj")
-
-    rm_list = [
-        cp0_app_elf,
-        cp0_app_bin,
-        cp0_elf_c_o,
-        cp1_app1_bin,
-        app_all_bin,
-        cp1_app_elf,
-        cp1_app_bin,
-        cp1_elf_c_o,
-    ]
-    for fil in rm_list:
-        rm_rf(fil)
-
+    rm_rf(build_root)
     print("Cleaning successful for T5AI.")
     return True
-
 
 
 def _get_tuya_libs_flag(param_data):
@@ -158,6 +133,86 @@ def copy_bin_file(build_root, target):
     return True
 
 
+def gen_bootloader_bin(root, build_root, target, assets_root):
+    '''
+/home/huatuo/tmp/arduino_package/output/1.1.4/vendor-t5_for_open/platform/T5AI/t5_os/bk_idk/tools/env_tools/beken_packager/cmake_Gen_image
+genfile
+-injsonfile
+/home/huatuo/tmp/arduino_package/output/1.1.4/vendor-t5_for_open/platform/T5AI/t5_os/bk_idk/tools/env_tools/beken_packager/config.json
+-infile
+/home/huatuo/tmp/arduino_package/output/1.1.4/vendor-t5_for_open/platform/T5AI/t5_os/bk_idk/components/bk_libs/bk7258/bootloader/normal_bootloader/bootloader.bin
+-outfile
+/home/huatuo/tmp/arduino_package/output/1.1.4/vendor-t5_for_open/platform/T5AI/t5_os/build/bk7258/bootloader.bin
+-genjson
+/home/huatuo/tmp/arduino_package/output/1.1.4/vendor-t5_for_open/platform/T5AI/t5_os/bk_idk/tools/env_tools/beken_packager/partition_bootloader.json
+    '''
+    tool_gen_image, _ = _get_packager_tools(assets_root)
+    config_file = os.path.join(
+        root, "t5_os", "bk_idk", "tools", "env_tools",
+        "beken_packager", "config.json"
+    )
+    bootload_bin = os.path.join(
+        root, "t5_os", "bk_idk", "components", "bk_libs",
+        target, "bootloader", "normal_bootloader",
+        "bootloader.bin")
+    out_bootload_bin = os.path.join(
+        build_root, target, "bootloader.bin"
+    )
+    json_file = os.path.join(
+        root, "t5_os", "bk_idk", "tools", "env_tools",
+        "beken_packager", "partition_bootloader.json"
+    )
+
+    cmd = f"{tool_gen_image} genfile -injsonfile {config_file}"
+    cmd += f" -infile {bootload_bin}"
+    cmd += f" -outfile {out_bootload_bin}"
+    cmd += f" -genjson {json_file}"
+
+    ret = do_subprocess(cmd)
+    if ret != 0:
+        print("Error: gen bootload.bin")
+        return False
+    return True
+
+
+def size_format(size, include_size):
+    if include_size:
+        for (val, suffix) in [(0x400, "K"), (0x100000, "M")]:
+            if size % val == 0:
+                return "%d%s" % (size // val, suffix)
+    return "0x%x" % size
+
+
+def shrink_val(val, need_shrink):
+    return int(val*32/34) if need_shrink else val
+
+
+def parse_int(v):
+    for letter, multiplier in [("k", 1024), ("m", 1024 * 1024)]:
+        if v.lower().endswith(letter):
+            return parse_int(v[:-1]) * multiplier
+        return int(v, 0)
+
+
+def gen_temp_configuration(asset_config, temp_config):
+    with open(asset_config, 'r') as local_json:
+        config_data = json.load(local_json)
+
+    start_addr = 0  # default
+    for sec in config_data['section']:
+        start_addr_tmp = shrink_val(
+                parse_int(sec["start_addr"]), True)
+        sec['start_addr'] = size_format(start_addr_tmp - start_addr, False)
+        sec['size'] = size_format(
+            shrink_val(
+                parse_int(sec["size"]), True), True)
+
+    temp_data_str = json.dumps(config_data, sort_keys=False, indent=4)
+    with open(temp_config, 'w') as f:
+        f.write(temp_data_str)
+    pass
+
+
 def _get_packager_tools(assets_root):
     packager_root = os.path.join(assets_root, "packager-tools")
     # "linux", "darwin_x86", "darwin_arm64", "windows"
@@ -187,35 +242,57 @@ def _get_packager_tools(assets_root):
     return tool_gen_image, tool_encrypt_crc
 
 
+# add 0xff padding to binary file
+def add_padding_to_binary(binary_file):
+    with open(binary_file, 'ab') as f:
+        f.write(bytes([0xff] * 34))  # 34 bytes align
+
+
+def check_and_padding_binary(app1_bin, app_pack_secs, app_crc_bin):
+    last_firmware_size = os.path.getsize(app1_bin)
+    last_partition_raw_size = parse_int("320K")
+    # 32 bytes align
+    last_firmware_aligned_size = (((last_firmware_size + 32 - 1) // 32) * 32)
+    # if the last firmware not fill up the partition, add padding value.
+    if last_firmware_aligned_size < last_partition_raw_size:
+        add_padding_to_binary(app_crc_bin)
+
+
 def gen_app_all_bin(root, build_root, target, assets_root):
     build_root = os.path.join(build_root, target)
-    partitions_file = os.path.join(
+    partitions_config = os.path.join(
         assets_root, target, "flags", "configuration.json")
+    temp_config = os.path.join(
+        build_root, "configuration_temp.json")
+    gen_temp_configuration(partitions_config, temp_config)
+
     bootload_bin = os.path.join(
-        root, "t5_os", "bk_idk", "components", "bk_libs",
-        "bk7258", "bootloader", "normal_bootloader",
-        "bootloader.bin")
+        build_root, "bootloader.bin")
     app_bin = os.path.join(build_root, "app.bin")
     app1_bin = os.path.join(build_root, "app1.bin")
     app_all_bin = os.path.join(build_root, "all-app.bin")
 
     tool_gen_image, tool_encrypt_crc = _get_packager_tools(assets_root)
 
-    cmd = f"{tool_gen_image} genfile -injsonfile {partitions_file}"
+    cmd = f"{tool_gen_image} genfile -injsonfile {temp_config}"
     cmd += f" -infile {bootload_bin} {app_bin} {app1_bin}"
     cmd += f" -outfile {app_all_bin}"
 
     ret = do_subprocess(cmd)
     if ret != 0:
-        print("Error: gen app-all.bin")
+        print("Error: gen all-app.bin")
         return False
 
     cmd = f"{tool_encrypt_crc} -crc {app_all_bin}"
 
     ret = do_subprocess(cmd)
     if ret != 0:
-        print("Error: crc app-all.bin")
+        print("Error: crc all-app.bin")
         return False
+
+    app_all_crc_bin = os.path.join(build_root, "all-app_crc.bin")
+    print(f"[Copy] {app_all_crc_bin} to {app_all_bin}.")
+    shutil.copy(app_all_crc_bin, app_all_bin)
 
     return True
 
@@ -227,6 +304,8 @@ def do_with_assets(root, build_root, user_cmd,
         clean(build_root, target)
         sys.exit(0)
 
+    os.makedirs(os.path.join(build_root, target), exist_ok=True)
+    os.makedirs(os.path.join(build_root, f"{target}_cp1"), exist_ok=True)
     assets_root = os.path.join(root, "tools", "vendor-t5_for_open")
     open_root = param_data["OPEN_ROOT"]
     toolchain_root = os.path.join(open_root, "platform", "tools",
@@ -251,6 +330,10 @@ def do_with_assets(root, build_root, user_cmd,
 
     # elf to bin: cp0 and cp1
     if not elf2bin(objcopy, target, build_root):
+        sys.exit(1)
+
+    # bootload.bin
+    if not gen_bootloader_bin(root, build_root, target, assets_root):
         sys.exit(1)
 
     copy_bin_file(build_root, target)
